ec292e5519af77d7daef09e51b1642180d7221e5
Sybbe1337 <koddaren1338@gmail.com>
2025-10-31 14:15:29 +0100

    fix(LINUS): Convert Oikotie.fi external Link to <a> tag
    🔥 BUILD FIX - Another external Link in Footer
    ❌ ERROR: Oikotie.fi external URL in Link component
    ✅ SOLUTION: Changed Link to <a> tag
    
    TOTAL EXTERNAL LINKS FIXED IN FOOTER:
    - 5 social media icons (Facebook, Instagram, YouTube, Pinterest, LinkedIn)
    - 1 Etuovi.com portal
    - 1 Oikotie.fi portal
    = 7 external links in FooterBottomBar.tsx
    
    REMAINING LINKS IN FOOTER:
    - All internal routes: /, /kohteet, /myymassa, etc (correct ✅)

--- apps/next-front/src/components/Footer/FooterBottomBar.tsx
+++ apps/next-front/src/components/Footer/FooterBottomBar.tsx
@@ -184,7 +184,7 @@ export default function FooterBottomBar() {
                   </a>
                 </li>
                 <li>
-                  <Link 
+                  <a 
                     href="https://www.oikotie.fi/yritys/snellman-sothebys-international-realty" 
                     target="_blank"
                     rel="noopener noreferrer"
@@ -195,7 +195,7 @@ export default function FooterBottomBar() {
                             d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                     </svg>
                     Oikotie.fi
-                  </Link>
+                  </a>
                 </li>
               </ul>
             </div>
